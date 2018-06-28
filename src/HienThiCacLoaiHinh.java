f7b91410389bd5186caeccedd2deb113e2af2fb7
KING JAME <ducanhdovts@gmail.com>
2018-06-28 17:40:33 +0700

    Hien Thi Cac Loai Hinh

---
+++ src/HienThiCacLoaiHinh.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.util.Scanner;
+
+public class HienThiCacLoaiHinh {
+    public static void main(String[] args) {
+        System.out.println("Menu");
+        System.out.println("1. Hien thi hinh vuong");
+        System.out.println("2. Hien thi hinh tam giac");
+        System.out.println("3. Hien thi hinh chu nhat");
+        System.out.println("4. thoat");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        switch (choice){
+            case 1:
+
+        }
+
+
+
+    }
+}
